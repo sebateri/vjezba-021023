f4fb654e198ece5672e2cb8475b09a40a89b7585
Sebastian Terihaj <sebastian.terihaj@gmail.com>
2023-10-02 18:22:15 +0200

    cijela vjezba

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [apodData, setApodData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY'
+        );
+        setApodData(response.data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (!apodData) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>NASA Astronomy Picture of the Day</h1>
+      <img src={apodData.url} alt={apodData.title} />
+      <h2>{apodData.title}</h2>
+      <p>{apodData.explanation}</p>
     </div>
   );
 }
